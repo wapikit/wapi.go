cba039c9b0565b0359200177dbd6858d476b3230
sarthakjdev <jsarthak448@gmail.com>
2025-09-01 12:23:18 +0530

    fix: add all necessary fields to phone number fetch calls
    Signed-off-by: sarthakjdev <jsarthak448@gmail.com>

--- manager/phone_number_manager.go
+++ manager/phone_number_manager.go
@@ -32,17 +32,116 @@ func NewPhoneNumberManager(config *PhoneNumberManagerConfig) *PhoneNumberManager
 	}
 }
 
+type WhatsappBusinessAccountPhoneNumberCodeVerificationStatus string
+
+const (
+	WhatsappBusinessAccountPhoneNumberCodeVerificationStatusVerified    WhatsappBusinessAccountPhoneNumberCodeVerificationStatus = "VERIFIED"
+	WhatsappBusinessAccountPhoneNumberCodeVerificationStatusNotVerified WhatsappBusinessAccountPhoneNumberCodeVerificationStatus = "NOT_VERIFIED"
+	WhatsappBusinessAccountPhoneNumberCodeVerificationStatusExpired     WhatsappBusinessAccountPhoneNumberCodeVerificationStatus = "EXPIRED"
+)
+
+type WhatsappBusinessAccountPhoneNumberMessagingLimitTier string
+
+const (
+	WhatsappBusinessAccountPhoneNumberMessagingLimitTierTier50        WhatsappBusinessAccountPhoneNumberMessagingLimitTier = "TIER_50"
+	WhatsappBusinessAccountPhoneNumberMessagingLimitTierTier250       WhatsappBusinessAccountPhoneNumberMessagingLimitTier = "TIER_250"
+	WhatsappBusinessAccountPhoneNumberMessagingLimitTierTier1K        WhatsappBusinessAccountPhoneNumberMessagingLimitTier = "TIER_1K"
+	WhatsappBusinessAccountPhoneNumberMessagingLimitTierTier10K       WhatsappBusinessAccountPhoneNumberMessagingLimitTier = "TIER_10K"
+	WhatsappBusinessAccountPhoneNumberMessagingLimitTierTier100K      WhatsappBusinessAccountPhoneNumberMessagingLimitTier = "TIER_100K"
+	WhatsappBusinessAccountPhoneNumberMessagingLimitTierTierUnlimited WhatsappBusinessAccountPhoneNumberMessagingLimitTier = "TIER_UNLIMITED"
+)
+
+type WhatsappBusinessAccountPhoneNumberNameStatus string
+
+const (
+	WhatsappBusinessAccountPhoneNumberNameStatusApproved               WhatsappBusinessAccountPhoneNumberNameStatus = "APPROVED"
+	WhatsappBusinessAccountPhoneNumberNameStatusAvailableWithoutReview WhatsappBusinessAccountPhoneNumberNameStatus = "AVAILABLE_WITHOUT_REVIEW"
+	WhatsappBusinessAccountPhoneNumberNameStatusDeclined               WhatsappBusinessAccountPhoneNumberNameStatus = "DECLINED"
+	WhatsappBusinessAccountPhoneNumberNameStatusExpired                WhatsappBusinessAccountPhoneNumberNameStatus = "EXPIRED"
+	WhatsappBusinessAccountPhoneNumberNameStatusNonExists              WhatsappBusinessAccountPhoneNumberNameStatus = "NON_EXISTS"
+	WhatsappBusinessAccountPhoneNumberNameStatusNone                   WhatsappBusinessAccountPhoneNumberNameStatus = "NONE"
+	WhatsappBusinessAccountPhoneNumberNameStatusPendingReview          WhatsappBusinessAccountPhoneNumberNameStatus = "PENDING_REVIEW"
+)
+
+type WhatsappBusinessAccountPhoneNumberPlatformType string
+
+const (
+	WhatsappBusinessAccountPhoneNumberPlatformTypeCloudApi      WhatsappBusinessAccountPhoneNumberPlatformType = "CLOUD_API"
+	WhatsappBusinessAccountPhoneNumberPlatformTypeNotApplicable WhatsappBusinessAccountPhoneNumberPlatformType = "NOT_APPLICABLE"
+	WhatsappBusinessAccountPhoneNumberPlatformTypeOnPremise     WhatsappBusinessAccountPhoneNumberPlatformType = "ON_PREMISE"
+)
+
+type WhatsappBusinessAccountPhoneNumberStatus string
+
+const (
+	WhatsappBusinessAccountPhoneNumberStatusPending      WhatsappBusinessAccountPhoneNumberStatus = "PENDING"
+	WhatsappBusinessAccountPhoneNumberStatusDeleted      WhatsappBusinessAccountPhoneNumberStatus = "DELETED"
+	WhatsappBusinessAccountPhoneNumberStatusMigrated     WhatsappBusinessAccountPhoneNumberStatus = "MIGRATED"
+	WhatsappBusinessAccountPhoneNumberStatusBanned       WhatsappBusinessAccountPhoneNumberStatus = "BANNED"
+	WhatsappBusinessAccountPhoneNumberStatusRestricted   WhatsappBusinessAccountPhoneNumberStatus = "RESTRICTED"
+	WhatsappBusinessAccountPhoneNumberStatusRateLimited  WhatsappBusinessAccountPhoneNumberStatus = "RATE_LIMITED"
+	WhatsappBusinessAccountPhoneNumberStatusFlagged      WhatsappBusinessAccountPhoneNumberStatus = "FLAGGED"
+	WhatsappBusinessAccountPhoneNumberStatusConnected    WhatsappBusinessAccountPhoneNumberStatus = "CONNECTED"
+	WhatsappBusinessAccountPhoneNumberStatusDisconnected WhatsappBusinessAccountPhoneNumberStatus = "DISCONNECTED"
+	WhatsappBusinessAccountPhoneNumberStatusUnknown      WhatsappBusinessAccountPhoneNumberStatus = "UNKNOWN"
+	WhatsappBusinessAccountPhoneNumberStatusUnverified   WhatsappBusinessAccountPhoneNumberStatus = "UNVERIFIED"
+)
+
+type WabaHealthStatus struct {
+	CanSendMessage string `json:"can_send_message"`
+	Entities       []struct {
+		EntityType        string `json:"entity_type"`
+		ID                string `json:"id"`
+		CanSendMessage    string `json:"can_send_message,omitempty"`
+		CanReceiveCallSIP string `json:"can_receive_call_sip,omitempty"`
+		Errors            []struct {
+			ErrorCode        int    `json:"error_code"`
+			ErrorDescription string `json:"error_description"`
+			PossibleSolution string `json:"possible_solution"`
+		} `json:"errors,omitempty"`
+	} `json:"entities"`
+}
+
+type WabaQualityScore struct {
+	Score string `json:"score"`
+}
+
+type WabaThroughput struct {
+	Level string `json:"level"`
+}
+
+type WabaOfficialBusinessAccount struct {
+	OBAStatus string `json:"oba_status"`
+}
+
 // WhatsappBusinessAccountPhoneNumber represents a WhatsApp Business Account phone number.
 type WhatsappBusinessAccountPhoneNumber struct {
-	VerifiedName           string `json:"verified_name,omitempty"`
-	DisplayPhoneNumber     string `json:"display_phone_number,omitempty"`
-	Id                     string `json:"id,omitempty"`
-	QualityRating          string `json:"quality_rating,omitempty"`
-	CodeVerificationStatus string `json:"code_verification_status,omitempty"`
-	Status                 string `json:"status,omitempty"` // CONNECTED
-	PlatformType           string `json:"platform_type,omitempty"`
-	CountryDialCode        string `json:"country_dial_code,omitempty"`
-	SearchVisibility       string `json:"search_visibility,omitempty"`
+	VerifiedName                          string                                                   `json:"verified_name,omitempty"`
+	DisplayPhoneNumber                    string                                                   `json:"display_phone_number,omitempty"`
+	Id                                    string                                                   `json:"id,omitempty"`
+	QualityRating                         string                                                   `json:"quality_rating,omitempty"`
+	CodeVerificationStatus                WhatsappBusinessAccountPhoneNumberCodeVerificationStatus `json:"code_verification_status,omitempty"`
+	Status                                WhatsappBusinessAccountPhoneNumberStatus                 `json:"status,omitempty"` // CONNECTED
+	PlatformType                          WhatsappBusinessAccountPhoneNumberPlatformType           `json:"platform_type,omitempty"`
+	CountryDialCode                       string                                                   `json:"country_dial_code,omitempty"`
+	SearchVisibility                      string                                                   `json:"search_visibility,omitempty"`
+	Certificate                           string                                                   `json:"certificate,omitempty"`
+	HealthStatus                          *WabaHealthStatus                                        `json:"health_status,omitempty"`
+	IsOfficialBusinessAccount             bool                                                     `json:"is_official_business_account,omitempty"`
+	IsOnBizApp                            bool                                                     `json:"is_on_biz_app,omitempty"`
+	IsPinEnabled                          bool                                                     `json:"is_pin_enabled,omitempty"`
+	IsPreverifiedNumber                   bool                                                     `json:"is_preverified_number,omitempty"`
+	LastOnboardedTime                     string                                                   `json:"last_onboarded_time,omitempty"`
+	MessagingLimitTier                    WhatsappBusinessAccountPhoneNumberMessagingLimitTier     `json:"messaging_limit_tier,omitempty"`
+	NameStatus                            WhatsappBusinessAccountPhoneNumberNameStatus             `json:"name_status,omitempty"`
+	NewCertificate                        string                                                   `json:"new_certificate,omitempty"`
+	NewDisplayName                        string                                                   `json:"new_display_name,omitempty"`
+	NewNameStatus                         WhatsappBusinessAccountPhoneNumberNameStatus             `json:"new_name_status,omitempty"`
+	EligibilityForApiBusinessGlobalSearch string                                                   `json:"eligibility_for_api_business_global_search,omitempty"`
+	AccountMode                           string                                                   `json:"account_mode,omitempty"`
+	Throughput                            *WabaThroughput                                          `json:"throughput,omitempty"`
+	OfficialBusinessAccount               *WabaOfficialBusinessAccount                             `json:"official_business_account,omitempty"`
+	QualityScore                          *WabaQualityScore                                        `json:"quality_score,omitempty"`
 }
 
 // WhatsappBusinessAccountPhoneNumberEdge represents a list of WhatsApp Business Account phone numbers.
@@ -56,6 +155,7 @@ type WhatsappBusinessAccountPhoneNumberEdge struct {
 func (manager *PhoneNumberManager) FetchAll(getSandBoxNumbers bool) (*WhatsappBusinessAccountPhoneNumberEdge, error) {
 	apiRequest := manager.requester.NewApiRequest(strings.Join([]string{manager.businessAccountId, "/", "phone_numbers"}, ""), http.MethodGet)
 
+	apiRequest.AddQueryParam("fields", "id,account_mode,certificate,code_verification_status,conversational_automation,display_phone_number,health_status,eligibility_for_api_business_global_search,id,is_official_business_account,is_on_biz_app,is_pin_enabled,is_preverified_number,last_onboarded_time,messaging_limit_tier,name_status,new_certificate,new_display_name,new_name_status,official_business_account,platform_type,quality_score,search_visibility,status,throughput,verified_name")
 	apiRequest.AddQueryParam("filtering", `[{"field":"account_mode","operator":"EQUAL","value":"LIVE"}]`)
 	response, err := apiRequest.Execute()
 
@@ -72,7 +172,7 @@ func (manager *PhoneNumberManager) FetchAll(getSandBoxNumbers bool) (*WhatsappBu
 // Fetch fetches a phone number by its ID.
 func (manager *PhoneNumberManager) Fetch(phoneNumberId string) (*WhatsappBusinessAccountPhoneNumber, error) {
 	apiRequest := manager.requester.NewApiRequest(phoneNumberId, http.MethodGet)
-
+	apiRequest.AddQueryParam("fields", "id,account_mode,certificate,code_verification_status,conversational_automation,display_phone_number,health_status,eligibility_for_api_business_global_search,id,is_official_business_account,is_on_biz_app,is_pin_enabled,is_preverified_number,last_onboarded_time,messaging_limit_tier,name_status,new_certificate,new_display_name,new_name_status,official_business_account,platform_type,quality_score,search_visibility,status,throughput,verified_name")
 	response, err := apiRequest.Execute()
 
 	if err != nil {
